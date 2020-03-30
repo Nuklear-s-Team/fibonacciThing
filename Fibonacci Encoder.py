e5fbca64f34da56b205556c9e2305783a023dd7d
Nuklear <56704842+PG-Development@users.noreply.github.com>
2020-03-30 15:01:02 -0400

    Updated main code
    Added repeated functionality, so you don't have to run the program over and over again

--- Fibonacci Encoder.py
+++ Fibonacci Encoder.py
@@ -18,54 +18,72 @@
 #Thanks! :)
 
 
-task = str(input("Decode or encode?"))
-totranslate = str(input("Give me the message."))
-keys = {" ": "|", "a": "2.", "b": "3.", "c": "5.", "d": "8.", "e": "13.", "f": "21.", "g": "34.", "h": "55.", "i": "89.", "j": "144.", "k": "233.", "l": "377.","m":"610.", "n": "987.", "o": "1597.", "p": "2584.", "q": "4181.", "r": "6765.", "s": "10946.",
-        "t": "17711.", "u": "28657.", "v": "46368.", "w": "75025.", "x": "121393.", "y": "196418.", "z": "317811.", "!": "!", ",": ",", "?": "?", "'": "'", "|": "|", ":": ":", "": ""}
-dekeys = {v: k for k, v in keys.items()}
-cdekeys = { k.replace('.', ''): v for k, v in dekeys.items() }
-if task == "en" or task == "encode" :
-    totranslate = totranslate.lower()
-    totranslate = totranslate.replace(".", "|||")
-    for item in totranslate:
-        totranslate = totranslate.replace(item, str(keys[item]))
-        totranslate = totranslate.replace(".|", "|")
-    if totranslate[len(totranslate)-1] == ".":
-        totranslate = totranslate[:len(totranslate)-1]
-if task == "de" or task == "decode" :
-    totranslate = totranslate.replace("|||", "~")
-    totranslate = totranslate.replace("|", " ")
 
-    translist = totranslate.split(sep=".")
-    for item in translist:
-        if " " in item:
-            index1 = translist.index(item)
-            temp = item.split(" ")
-            for item in temp:
-                if "~" in item:
-                    temp2 = item.replace("~", "")                 
-                    tempIndex = temp.index(item)
-                    temp[tempIndex] = temp2
-                    temp2 = str(cdekeys[temp2])#excuse my messy code here
-                    temp[tempIndex] = temp2
-                    temp2 = temp2 + ". "
-                    temp[tempIndex] = temp2
-                else:
-                    index2 = temp.index(item)
-                    item = str(cdekeys[item])
-                    temp[index2] = item
-    
-            item = " ".join(temp)
-            translist[index1] = item
-        elif "~" in item:
-            temp = item.replace("~", "")
-            temp = str(cdekeys[temp])
-            index = translist.index(item)
-            item = temp + ". "
-            translist[index] = item
-        else:
-            index = translist.index(item)
-            translist[index] = item.replace(item, str(cdekeys[item]))
-    totranslate = "".join(translist)
+complete = False
 
-print(totranslate)
+
+def translate():
+    keys = {" ": "|", "a": "2.", "b": "3.", "c": "5.", "d": "8.", "e": "13.", "f": "21.", "g": "34.", "h": "55.",
+            "i": "89.", "j": "144.", "k": "233.", "l": "377.", "m": "610.", "n": "987.", "o": "1597.", "p": "2584.",
+            "q": "4181.", "r": "6765.", "s": "10946.",
+            "t": "17711.", "u": "28657.", "v": "46368.", "w": "75025.", "x": "121393.", "y": "196418.", "z": "317811.",
+            "!": "!", ",": ",", "?": "?", "'": "'", "|": "|", ":": ":", "": ""}
+    dekeys = {v: k for k, v in keys.items()}
+    cdekeys = {k.replace('.', ''): v for k, v in dekeys.items()}
+    task = str(input("Decode or encode?"))
+    totranslate = str(input("Give me the message."))
+    ask = "lol"
+    if task == "en" or task == "encode":
+        totranslate = totranslate.lower()
+        totranslate = totranslate.replace(".", "|||")
+        for item in totranslate:
+            totranslate = totranslate.replace(item, str(keys[item]))
+            totranslate = totranslate.replace(".|", "|")
+        if totranslate[len(totranslate) - 1] == ".":
+            totranslate = totranslate[:len(totranslate) - 1]
+    elif task == "de" or task == "decode":
+        totranslate = totranslate.replace("|||", "~")
+        totranslate = totranslate.replace("|", " ")
+
+        translist = totranslate.split(sep=".")
+        for item in translist:
+            if " " in item:
+                index1 = translist.index(item)
+                temp = item.split(" ")
+                for item in temp:
+                    if "~" in item:
+                        temp2 = item.replace("~", "")
+                        tempIndex = temp.index(item)
+                        temp[tempIndex] = temp2
+                        temp2 = str(cdekeys[temp2])  # excuse my messy code here
+                        temp[tempIndex] = temp2
+                        temp2 = temp2 + ". "
+                        temp[tempIndex] = temp2
+                    else:
+                        index2 = temp.index(item)
+                        item = str(cdekeys[item])
+                        temp[index2] = item
+
+                item = " ".join(temp)
+                translist[index1] = item
+            elif "~" in item:
+                temp = item.replace("~", "")
+                temp = str(cdekeys[temp])
+                index = translist.index(item)
+                item = temp + ". "
+                translist[index] = item
+            else:
+                index = translist.index(item)
+                translist[index] = item.replace(item, str(cdekeys[item]))
+        totranslate = "".join(translist)
+    print(totranslate)
+    ask = input("Another task? Y or N")
+    return ask
+
+
+while complete is False:
+    ask2 = translate()
+    if ask2 == "Yes" or ask2 == "Y":
+        translate()
+    else:
+        complete = True
