a154dd93cc861f10b45b5b20d2d41c250cc1f7c1
Nuklear <56704842+PG-Development@users.noreply.github.com>
2020-04-03 12:05:20 -0400

    Made improvements to the GUI
    Added colors and dividers

--- Fibonnacci GUI VERSION.py
+++ Fibonnacci GUI VERSION.py
@@ -31,14 +31,36 @@ def translate():
     T.insert(tkinter.END, totranslate)
 
 
-Encode = tkinter.Radiobutton(root, text='Encode', variable=lol, value="Encode").grid(row=0, sticky=tkinter.W)
-Decode = tkinter.Radiobutton(root, text='Decode', variable=lol, value="Decode").grid(row=1, sticky=tkinter.W)
-Regular = tkinter.Radiobutton(root, text='Regular', variable=mode, value="Regular").grid(row=0, column=0, sticky=tkinter.E)
-Reversed = tkinter.Radiobutton(root, text='Reversed', variable=mode, value="Reversed").grid(row=1, column=0, sticky=tkinter.E)
+Encode = tkinter.Radiobutton(root, text='Encode', variable=lol, value="Encode", indicatoron=0, width=42, selectcolor="light green").grid(row=1, column=0, sticky=tkinter.W)
+Decode = tkinter.Radiobutton(root, text='Decode', variable=lol, value="Decode", indicatoron=0, width=42, selectcolor="light green").grid(row=1, column=0, sticky=tkinter.E)
+Regular = tkinter.Radiobutton(root, text='Regular', variable=mode, value="Regular", indicatoron=0, width=42, selectcolor="cyan").grid(row=3, column=0, sticky=tkinter.W)
+Reversed = tkinter.Radiobutton(root, text='Reversed', variable=mode, value="Reversed", indicatoron=0, width=42, selectcolor="cyan").grid(row=3, column=0, sticky=tkinter.E)
+
+J = tkinter.Scale(root, state=tkinter.DISABLED, length=600, troughcolor="black", width=1, orient=tkinter.HORIZONTAL, showvalue=0, sliderlength=200)
+J.set(100)
+J.grid(row=0, sticky=tkinter.N)
+
+R = tkinter.Scale(root, state=tkinter.DISABLED, length=500, troughcolor="black", width=1, orient=tkinter.HORIZONTAL, showvalue=0, sliderlength=200)
+R.set(100)
+R.grid(row=2, sticky=tkinter.N)
+
 
 T = tkinter.Entry(root, width=100)
-T.grid(row=2, column=0, sticky=tkinter.W)
-tkinter.Button(root, text="Translate", command=translate, width=43).grid(row=3, column=0, sticky=tkinter.W)
-tkinter.Button(root, text="Quit", command=root.quit, width=43).grid(row=3, column=0, sticky=tkinter.E)
+L = tkinter.Scale(root, state=tkinter.DISABLED, length=500, troughcolor="black", width=1, orient=tkinter.HORIZONTAL, showvalue=0, sliderlength=200)
+L.set(100)
+L.grid(row=4, sticky=tkinter.N)
+T.grid(row=5, column=0, sticky=tkinter.W)
+
+X = tkinter.Scale(root, state=tkinter.DISABLED, length=500, troughcolor="black", width=1, orient=tkinter.HORIZONTAL, showvalue=0, sliderlength=200)
+X.set(100)
+X.grid(row=6, sticky=tkinter.N)
+
+Translate = tkinter.Button(root, text="Translate", command=translate, width=42, activebackground="light green").grid(row=7, column=0, sticky=tkinter.W)
+Quit = tkinter.Button(root, text="Quit", command=root.quit, width=42, activebackground="red").grid(row=7, column=0, sticky=tkinter.E)
 T.insert(tkinter.END, "")
+
+X = tkinter.Scale(root, state=tkinter.DISABLED, length=600, troughcolor="black", width=1, orient=tkinter.HORIZONTAL, showvalue=0, sliderlength=200)
+X.set(100)
+X.grid(row=8, sticky=tkinter.N)
+
 tkinter.mainloop()
