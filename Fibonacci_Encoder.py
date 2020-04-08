25e16d7fdb38cc02688059ecdf71d039923bc1fb
Nuklear <56704842+PG-Development@users.noreply.github.com>
2020-04-08 11:54:23 -0400

    Add files via upload

--- Fibonacci_Encoder.py
+++ Fibonacci_Encoder.py
@@ -18,6 +18,8 @@
 # Please credit me as the original author if you are making something using this.
 # Thanks! :)
 
+import random
+
 keys = {" ": "|", "a": "2.", "b": "3.", "c": "5.", "d": "8.", "e": "13.", "f": "21.", "g": "34.", "h": "55.",
         "i": "89.", "j": "144.", "k": "233.", "l": "377.", "m": "610.", "n": "987.", "o": "1597.", "p": "2584.",
         "q": "4181.", "r": "6765.", "s": "10946.", "t": "17711.", "u": "28657.", "v": "46368.", "w": "75025.",
@@ -35,6 +37,21 @@ reversedkeys = {" ": "|", "a": "317811.", "b": "196418.", "c": "121393.", "d": "
 
 dereversedkeys = {v: k for k, v in reversedkeys.items()}
 cdereversedkeys = {k.replace('.', ''): v for k, v in dereversedkeys.items()}
+
+availableKeysBase = ["2.", "3.", "5.", "8.", "13.", "21.", "34.", "55.", "89.", "144.", "233.", "377.", "610.", "987.", "1597.", "2584.",
+                     "4181.", "6765.", "10946.", "17711.", "28657.", "46368.", "75025.", "121393.", "196418.", "317811."]
+
+availableKeys = ["2.", "3.", "5.", "8.", "13.", "21.", "34.", "55.", "89.", "144.", "233.", "377.", "610.", "987.", "1597.", "2584.",
+                     "4181.", "6765.", "10946.", "17711.", "28657.", "46368.", "75025.", "121393.", "196418.", "317811."]
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+keyList = []
+
+symbolsDict = {" ": "|", "!": "!", ",": ",", "?": "?", "'": "'.", "|": "|", ":": ":", "": "", "-": "-."}
+
+randomDict = {}
+
 
 def encode(message):
     totranslate = message.lower()
@@ -130,6 +147,76 @@ def decodeReversed(message):
     return "".join(translist)
 
 
+def randomGen():
+    randomDict = {}
+    for b in letters:
+        randomNum=random.randint(0, len(availableKeys)-1)
+        randomDict[b] = availableKeys[randomNum]
+        keyList.append(str(availableKeysBase.index(availableKeys[randomNum])))
+        availableKeys.pop(randomNum)
+    randomDict.update(symbolsDict)
+    finalKey = ",".join(keyList)
+    return randomDict, finalKey
+
+
+def encodeRandom(message, randomDict):
+    totranslate = message.lower()
+    totranslate = totranslate.replace(".", "|||")
+    for item in totranslate:
+        totranslate = totranslate.replace(item, str(randomDict[item]))
+        totranslate = totranslate.replace(".|", "|")
+    if totranslate[-1] == ".":
+        totranslate = totranslate[:len(totranslate) - 1]
+    return totranslate
+
+
+def generatefromkey(key4):
+    dic = {}
+    listOfKeys = key4.split(",")
+    for w in letters:
+        dic[w] = availableKeysBase[int(listOfKeys[letters.index(w)])]
+        dic.update(symbolsDict)
+    return dic
+
+def decodeRandom(message, dict):
+    randomdekeys = {v: k for k, v in dict.items()}
+    randomcdekeys = {k.replace('.', ''): v for k, v in randomdekeys.items()}
+    totranslate = message.replace("|||", "~")
+    totranslate = totranslate.replace("|", " ")
+    translist = totranslate.split(sep=".")
+    for item in translist:
+        if " " in item:
+            index1 = translist.index(item)
+            temp = item.split(" ")
+            for item in temp:
+                if "~" in item:
+                    temp2 = item.replace("~", "")
+                    tempIndex = temp.index(item)
+                    temp[tempIndex] = temp2
+                    temp2 = str(randomcdekeys[temp2])  # excuse my messy code here
+                    temp[tempIndex] = temp2
+                    temp2 = temp2 + ". "
+                    temp[tempIndex] = temp2
+                else:
+                    index2 = temp.index(item)
+                    item = str(randomcdekeys[item])
+                    temp[index2] = item
+
+            item = " ".join(temp)
+            translist[index1] = item
+        elif "~" in item:
+            temp = item.replace("~", "")
+            temp = str(randomcdekeys[temp])
+            index = translist.index(item)
+            item = temp + ". "
+            translist[index] = item
+        else:
+            index = translist.index(item)
+            translist[index] = item.replace(item, str(randomcdekeys[item]))
+    return "".join(translist)
+
+
+
 if __name__ == '__main__':  # executed only if you use the file directly and not by importing it
     complete = False
     decodeNames = ["de", "decode", "De", "Decode", "d", "D"]
@@ -137,9 +224,27 @@ if __name__ == '__main__':  # executed only if you use the file directly and not
     while complete == False:
         task = str(input("Decode or encode?"))
         if task in encodeNames:
-            print("Give me your message")
-            message = str(input(""))
-            print(encode(message))
+            print("Random of not? Y or N")
+            i = input()
+            if i in ["Yes", 'y', 'yes', 'Y']:
+                print("Give me the message")
+                message = input()
+                print("Give me the key [just press enter if you want to generate it randomly]")
+                key = input()
+                if key == '':
+                    randomDict, key = randomGen()
+                    print("your key is {}".format(key))
+                    print("And your message is:")
+                    print(encodeRandom(message, randomDict))
+                else:
+                    print(encodeRandom(message, generatefromkey(key)))
+
+
+
+            else:
+                print("Give me your message")
+                message = str(input(""))
+                print(encode(message))
 
         elif task in decodeNames:
             print("Give me your message")
--- Fibonnacci GUI VERSION.py
+++ Fibonnacci GUI VERSION.py
@@ -6,26 +6,42 @@
 import tkinter as tkinter
 from tkinter import messagebox
 from tkinter import scrolledtext
-import time
-from Fibonacci_Encoder import encode, decode, encodeReversed, decodeReversed
+from Fibonacci_Encoder import encode, decode, encodeReversed, decodeReversed, randomGen, encodeRandom, generatefromkey, decodeRandom
 root = tkinter.Tk(className="Fibonacci Encoder")
+help = tkinter.Toplevel(width=90, height=90)
+help.withdraw()
 lol = tkinter.StringVar()
 mode = tkinter.StringVar()
 lastTranslation = tkinter.StringVar()
+key = tkinter.StringVar()
 lastTranslation.set("Last Tranlsation: None")
 root.iconphoto(False, tkinter.PhotoImage(file="icon.png"))
 decodeNames = ["de", "decode", "De", "Decode", "d", "D"]
 encodeNames = ["en", "encode", "En", "Encode", "e", "E"]
 
 
+def showHelp():
+    help.deiconify()
+
+
+def closeHelp():
+    help.withdraw()
+
+
 def error():
     messagebox.showerror("Error", "Invalid Input")
+
+
+def keyMissingError():
+    messagebox.showerror("Error", "No Key")
 
 
 def translate():
     task = lol.get()
     message = T.get()
     version = mode.get()
+    getKey = RandomEncode.get()
+    global key
     T.delete(0, tkinter.END)
     if task in encodeNames:
         if version == "Regular":
@@ -36,6 +52,20 @@ def translate():
         elif version == "Reversed":
             try:
                 totranslate = encodeReversed(message)
+            except BaseException:
+                error()
+        elif version == "Random":
+            try:
+                if getKey == "":
+                    randomDict, key2 = randomGen()
+                    key.set(key2)
+                    totranslate = encodeRandom(message, randomDict)
+                    getKey = ""
+                    randomDict = {}
+                    key2 = ""
+                else:
+                    totranslate = encodeRandom(message, generatefromkey(getKey))
+                    getKey = ""
             except BaseException:
                 error()
     elif task in decodeNames:
@@ -49,46 +79,76 @@ def translate():
                 totranslate = decodeReversed(message)
             except BaseException:
                 error()
+        elif version == "Random":
+            if getKey == "":
+                keyMissingError()
+            else:
+                totranslate = decodeRandom(message, generatefromkey(getKey))
     T.insert(tkinter.END, totranslate)
     link.delete(1.0, tkinter.END)
-    link.insert(1.0, "Last Translation: " + message + " <-> " + totranslate)
+    keyDisplay.delete(1.0, tkinter.END)
+    mode2 = mode.get()
+    link.insert(1.0, "Last Translation: (" + mode2 + " Mode) "+ message + " <-> " + totranslate)
+    keyDisplay.insert(1.0, key.get())
 
 
 Encode = tkinter.Radiobutton(root, text='Encode', variable=lol, value="Encode", indicatoron=0, width=42, selectcolor="light green").grid(row=1, column=0, sticky=tkinter.W)
-Decode = tkinter.Radiobutton(root, text='Decode', variable=lol, value="Decode", indicatoron=0, width=42, selectcolor="light green").grid(row=1, column=0, sticky=tkinter.E)
-Regular = tkinter.Radiobutton(root, text='Regular', variable=mode, value="Regular", indicatoron=0, width=42, selectcolor="cyan").grid(row=3, column=0, sticky=tkinter.W)
-Reversed = tkinter.Radiobutton(root, text='Reversed', variable=mode, value="Reversed", indicatoron=0, width=42, selectcolor="cyan").grid(row=3, column=0, sticky=tkinter.E)
-
+Decode = tkinter.Radiobutton(root, text='Decode', variable=lol, value="Decode", indicatoron=0, width=42, selectcolor="light green").grid(row=1, column=1, sticky=tkinter.E)
+Regular = tkinter.Radiobutton(root, text='Regular', variable=mode, value="Regular", indicatoron=0, width=42, selectcolor="cyan").grid(row=4, column=0, sticky=tkinter.W)
+Reversed = tkinter.Radiobutton(root, text='Reversed', variable=mode, value="Reversed", indicatoron=0, width=42, selectcolor="cyan").grid(row=4, column=1, sticky=tkinter.E)
+Random = tkinter.Radiobutton(root, text="Use Random Dictionary", variable=mode, value="Random", indicatoron=0, width=42, selectcolor="gold").grid(row=5, column=0, sticky=tkinter.N)
+RandomEncode = tkinter.Entry(root, width=48)
+RandomEncode.grid(row=5, column=1)
 J = tkinter.Scale(root, state=tkinter.DISABLED, length=600, troughcolor="black", width=1, orient=tkinter.HORIZONTAL, showvalue=0, sliderlength=200, label="--------------------------------------------------------Mode--------------------------------------------------------")
 J.set(100)
-J.grid(row=0, sticky=tkinter.N)
+J.grid(row=0, sticky=tkinter.N, columnspan=2)
 
 R = tkinter.Scale(root, state=tkinter.DISABLED, length=500, troughcolor="black", width=1, orient=tkinter.HORIZONTAL, showvalue=0, sliderlength=200, label="--------------------------------------------Key Sets--------------------------------------------")
 R.set(100)
-R.grid(row=2, sticky=tkinter.N)
+R.grid(row=3, sticky=tkinter.N, columnspan=2)
 
 T = tkinter.Entry(root, width=100)
 L = tkinter.Scale(root, state=tkinter.DISABLED, length=500, troughcolor="black", width=1, orient=tkinter.HORIZONTAL, showvalue=0, sliderlength=200, label="------------------------------------------Input Below------------------------------------------")
 L.set(100)
-L.grid(row=5, sticky=tkinter.N)
-T.grid(row=6, column=0, sticky=tkinter.W)
+L.grid(row=6, sticky=tkinter.N, columnspan=2)
+T.grid(row=7, column=0, sticky=tkinter.W, columnspan=2)
 
 X = tkinter.Scale(root, state=tkinter.DISABLED, length=500, troughcolor="black", width=1, orient=tkinter.HORIZONTAL, showvalue=0, sliderlength=200)
 X.set(100)
-X.grid(row=7, sticky=tkinter.N)
+X.grid(row=8, sticky=tkinter.N, columnspan=2)
 
-Translate = tkinter.Button(root, text="Translate", command=translate, width=42, activebackground="light green").grid(row=8, column=0, sticky=tkinter.W)
-Quit = tkinter.Button(root, text="Quit", command=root.quit, width=42, activebackground="red").grid(row=8, column=0, sticky=tkinter.E)
+Translate = tkinter.Button(root, text="Translate", command=translate, width=42, activebackground="light green").grid(row=9, column=0, sticky=tkinter.W)
+Quit = tkinter.Button(root, text="Quit", command=root.quit, width=85, activebackground="red").grid(row=10, columnspan=2, sticky=tkinter.E)
 T.insert(tkinter.END, "")
+Help = tkinter.Button(root, text="Help", command=showHelp, width=42, activebackground="blue").grid(row=9, column=1)
 
 X = tkinter.Scale(root, state=tkinter.DISABLED, length=600, troughcolor="black", width=1, orient=tkinter.HORIZONTAL, showvalue=0, sliderlength=200)
 X.set(100)
-X.grid(row=9, sticky=tkinter.N)
+X.grid(row=11, sticky=tkinter.N, columnspan=2)
 
-link = scrolledtext.ScrolledText(root, width=74, height=5, wrap="word")
-link.grid(row=10, sticky=tkinter.W)
+link = scrolledtext.ScrolledText(root, width=65, height=5, wrap="word", font="consolas")
+link.grid(row=12, sticky=tkinter.W, columnspan=2)
 
-info = tkinter.Label(root, text="https://github.com/PG-Development/Fibonacci-Encoder")
-info.grid(row=11, sticky=tkinter.E)
+keyDisplay = tkinter.Text(root, height=1, width=75, wrap="word")
+keyDisplay.grid(row=13, sticky=tkinter.W, columnspan=2)
+
+info = tkinter.Label(root, text="Created by Khang Nguyen and Luzgog. Github link: https://github.com/PG-Development/Fibonacci-Encoder")
+info.grid(row=14, sticky=tkinter.E, columnspan=2)
+
+# help window below
+
+title = tkinter.Label(help, text="Help Menu").grid(row=0, sticky=tkinter.N)
+helpText = scrolledtext.ScrolledText(help, width=65, height=20, wrap="word")
+helpText.grid(row=1, sticky=tkinter.N)
+helpText.insert(1.0, "Thanks for downloading this encoder! My team and I have worked hard on it. \n \n "
+                     " Important: When you want to close this, do NOT press the x at the top right. Press the exit help menu button at the bottom. \n \n"
+                     "When you download this repository, you should have gotten 2 .py files: the Fibonacci_Encoder, and the Fibonacci GUI Version file. "
+                     "Make sure they are in the same folder. The GUI Version is the much more convenient version of this program, but it uses the same functions. "
+                     "When you open up the GUI file, you should see a small window pop up on your screen. This is the main application window, built with tkinter. "
+                     "You select your mode at the top, choosing from either Encode, Decode, or Decode from Random. To encode from a random, select Encode from random in the keysets. "
+                     "Below those, you should see keysets. You have regular, reversed, and encode from random. Next to both of the random choices you see inputs. "
+                     "Below the modes and keysets you see an input, to place your text inside, and this is where the message will come out. ")
+closeHelpButton = tkinter.Button(help, text="Exit Help Menu", command=closeHelp, activebackground="red").grid(row=2, sticky=tkinter.N)
+
 
 tkinter.mainloop()
